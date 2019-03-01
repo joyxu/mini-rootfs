b7bb1e35c7531baf112935c1c48c4ce768d75238
Wei Xu <xuwei5@hisilicon.com>
2019-03-01 10:32:22 +0000

    add pvscan, python
    Signed-off-by: Wei Xu <xuwei5@hisilicon.com>

--- cp-dep.sh
+++ cp-dep.sh
@@ -5,3 +5,5 @@ for library in $(ldd "${T}" | cut -d '>' -f 2 | awk '{print $1}')
 do
 	[ -f "${library}" ] && cp -farpv --parents "${library}"* "${D}"
 done
+
+find ${D} -type l -! -exec test -e {} \; -print | xargs rm
